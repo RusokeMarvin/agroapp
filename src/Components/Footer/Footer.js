535ea895e547adbcb60ee15caf3aa6923eb0f84f
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-12-12 16:28:00 +0300

    agroapp

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,13 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='footers'>
-        <footer>hello</footer>
+        <footer>
+          <ul>
+            <h1>Contact information.</h1>
+            <li>Conatact:+256780206512 / +256741750372</li>
+            <li>Email: admin@awciinternational.org / info@awciinternational.org</li>
+          </ul>
+</footer>
     </div>
   )
 }
