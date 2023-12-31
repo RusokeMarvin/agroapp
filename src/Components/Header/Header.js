a5c8786b11b8b6dbdbd9910d303a37c81cfe0c79
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-12-31 13:46:06 +0300

    UPDATE

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,7 +17,7 @@ function Header() {
   return (
     <div className='header'>
       <nav>
-      <h1>AWCI-AGRICULTUR  </h1> 
+      <h1>AWCI-AGRICULTURE  </h1> 
       <img src={ourlogo}/>
       
       <ul className='sidebar'>
